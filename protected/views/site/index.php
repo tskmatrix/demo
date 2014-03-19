03be4650d3d62b5a6c3a09d368406b80b6e4dc2e
Tom King <tom@tskmatrix.com>
2014-03-19 11:27:19 -0500

    modified the carousel

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -5,7 +5,7 @@ $this->pageTitle=Yii::app()->name;
 ?>
 
 <?php echo TbHtml::carousel(array(
-    array('image' => Yii::app()->request->baseUrl . '/images/areca-multi-purpose-responsive-magento-theme-magento1.png', 'label' => 'First Thumbnail lable', 'caption' => '...'),
-    array('image' => Yii::app()->request->baseUrl . '/images/social-media-campaign-steps.png', 'label' => 'Second Thumbnail label', 'caption' => '...'),
-    array('image' => Yii::app()->request->baseUrl . '/images/yii_hosting.png', 'label' => 'Third Thumbnail label', 'caption' => '...'),
+    array('image' => Yii::app()->request->baseUrl . '/images/areca-multi-purpose-responsive-magento-theme-magento1.png', 'label' => 'This site is built with', 'caption' => 'CSS3, HTML5, jQuery,and the YiiStrap Extension'),
+    array('image' => Yii::app()->request->baseUrl . '/images/social-media-campaign-steps.png', 'label' => 'Social Media', 'caption' => 'Campaign steps'),
+    array('image' => Yii::app()->request->baseUrl . '/images/yii_hosting.png', 'label' => 'Yii Framework', 'caption' => 'A PHP MVC programming framework'),
 )); ?>
